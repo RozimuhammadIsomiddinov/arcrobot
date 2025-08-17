f8c4be74e7a2b4f7c0359bc9c13249ae16269aae
RozimuhammadIsomiddinov <roziisomiddinov@gmail.com>
2025-08-17 16:06:58 +0500

    removed any coments

--- middleware/multer.js
+++ middleware/multer.js
@@ -6,17 +6,15 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Fayllar saqlanadigan joy
 const uploadPath = path.resolve(__dirname, "..", "public/images");
 
-// Agar katalog mavjud bo‘lmasa, uni yaratamiz
 if (!fs.existsSync(uploadPath)) {
   fs.mkdirSync(uploadPath, { recursive: true });
 }
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, uploadPath); // To‘g‘ridan-to‘g‘ri `public/images/` ichiga saqlaymiz
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     const uniqueName = `${file.originalname.replace(/\s+/g, "")}`;
